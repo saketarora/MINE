8fede4b4a6b47e823530477b2b9cec8720825299
saketarora <saketarora88@yahoo.com>
2018-02-21 09:06:17 -0800

    unit 6 part 2

---
+++ Lab08g.java
@@ -0,0 +1,26 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab08g
+{
+	public static void main ( String[] args )
+	{
+		LoopStats LS = new LoopStats();
+		LS.setNums(1,5);
+		LS.getEvenCount();
+		LS.getOddCount();
+		LS.getTotal();
+		System.out.println(LS.toString());
+		
+		
+		
+		
+		
+					
+	}
+}
