82513717b28a7ae3cd33508152d2b03ba51d9ae6
saketarora <36201615+saketarora@users.noreply.github.com>
2018-03-19 09:27:53 -0700

    Lab 18d

--- Lab18d.java
+++ Lab18d.java
@@ -15,16 +15,20 @@ public class Lab18d
 {
 	public static void main( String args[] ) throws IOException
 	{
-		ArrayList house = new ArrayList();
+		ArrayList<Word> house = new ArrayList();
 		Scanner file = new Scanner(new File("C:\\Users\\Saket\\Documents\\JAVA Work\\12b\\src\\lab18d.dat"));
-		Word sort = new Word();
+	
 		int size = file.nextInt();
 		for (int i = 0; i < size; i++)
 		{
 			house.add(file.nextLine());
 		}
 		for (int i = 0; i < house.size(); i++) {
-			
+			String in = house.get(i);
+			Word sort = new Word(in);
+			for (int j = i +1; j < house.size(); j++ ){
+				sort.compareTo(house.get(j));
+			}
 		}
 		
 
